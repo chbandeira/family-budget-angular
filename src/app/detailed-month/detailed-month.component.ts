05bb791fc81414405901069fe02e1c3649600b5f
Charlles Bandeira <charllesbandeira@gmail.com>
2021-05-28 13:44:46 -0400

    add version, category details

--- src/app/detailed-month/detailed-month.component.ts
+++ src/app/detailed-month/detailed-month.component.ts
@@ -32,6 +32,8 @@ export class DetailedMonthComponent implements OnInit {
   month = 0;
   months = MonthConst.MONTHS;
 
+  categories = new Array(5);
+
   constructor(
     private incomeService: IncomeService,
     private expenseService: ExpenseService,
@@ -73,7 +75,7 @@ export class DetailedMonthComponent implements OnInit {
         },
       },
       legend: {
-        position: 'right'
+        position: 'bottom'
       }
     };
     this.barChartLabels = [''];
@@ -108,29 +110,27 @@ export class DetailedMonthComponent implements OnInit {
 
       // Basics
       const basics = [CategoryConst.LIST[0], CategoryConst.LIST[1], CategoryConst.LIST[9]];
-      this.setValueBarChartDataItem(0, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => basics.includes(e.category)));
+      this.setValueBarChartDataItem(0, basics, totalExpense, isAverage, expenses.data);
 
       // Entertainment
-      this.setValueBarChartDataItem(1, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => CategoryConst.LIST[2] === e.category));
+      const entertainment = [CategoryConst.LIST[2]];
+      this.setValueBarChartDataItem(1, entertainment, totalExpense, isAverage, expenses.data);
 
       // Grocery
-      this.setValueBarChartDataItem(2, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => CategoryConst.LIST[3] === e.category));
+      const grocery = [CategoryConst.LIST[3]];
+      this.setValueBarChartDataItem(2, grocery, totalExpense, isAverage, expenses.data);
 
       // Home
       const home = [CategoryConst.LIST[4], CategoryConst.LIST[5], CategoryConst.LIST[7], CategoryConst.LIST[10], CategoryConst.LIST[11]];
-      this.setValueBarChartDataItem(3, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => home.includes(e.category)));
+      this.setValueBarChartDataItem(3, home, totalExpense, isAverage, expenses.data);
 
       // Investments
-      this.setValueBarChartDataItem(4, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => CategoryConst.LIST[6] === e.category));
+      const investments = [CategoryConst.LIST[6]];
+      this.setValueBarChartDataItem(4, investments, totalExpense, isAverage, expenses.data);
 
       // Miscellaneous
-      this.setValueBarChartDataItem(5, totalExpense, isAverage, expenses.data
-        .filter((e: Expense) => CategoryConst.LIST[8] === e.category));
+      const miscellaneous = [CategoryConst.LIST[8]];
+      this.setValueBarChartDataItem(5, miscellaneous, totalExpense, isAverage, expenses.data);
 
       // TOTAL
       combineLatest([
@@ -150,10 +150,17 @@ export class DetailedMonthComponent implements OnInit {
 
   /**
    * Set an item value in the barChartData
-   * @param index Index of the expense
+   * @param index Index of the category group
+   * @param categoryFilters Categories to filter expenses
+   * @param totalExpenses Total of expenses
+   * @param isAverage true if want to get the average; othewise false
    * @param expenses Array of expenses
    */
-  setValueBarChartDataItem(index: number, totalExpenses: number, isAverage: boolean, expenses: Expense[]) {
+  setValueBarChartDataItem(index: number, categoryFilters: string[], totalExpenses: number, isAverage: boolean, expenses: Expense[]) {
+    expenses = expenses.filter((e: Expense) => categoryFilters.includes(e.category));
+    this.categories[index] = {group: CategoryConst.GROUP[index], categories: []};
+    this.categories[index]['categories'].push(categoryFilters.join(', '));
+
     let sum = 0;
     expenses.forEach((e: Expense) => sum += e.value);
     if (isAverage) {
